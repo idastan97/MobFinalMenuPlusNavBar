30ab1da1772c83bc20f96b4fc1128ef247b26e04
baktybekdoskul <baktybek.doskul@gmail.com>
2020-04-21 15:45:22 +0600

    Options menu removed

--- app/src/main/java/com/example/mobfinalmenuplusnavbar/MainActivity.java
+++ app/src/main/java/com/example/mobfinalmenuplusnavbar/MainActivity.java
@@ -57,12 +57,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public void onBackPressed() {
